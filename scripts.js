7ee239c844bb2a57712c655d50498d1972481511
mofodreamer <mofodr@protonmail.com>
2024-01-11 16:58:21 +0100

    add pixel over hover

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-let columns = 4;
-let rows = 4;
+squareNumber=prompt ("How many squares per side?")
+if (window.screen.width   <= 1000 && squareNumber > 51) {
+    prompt("Number should be under 50. Choose a new number.");
+} else if (window.screen.width  >= 1001 && squareNumber > 101) {
+    prompt("Number should be under 100. Choose a new number.");
+}
+
+let columns = squareNumber;
+let rows = squareNumber;
 
 let grid = document.createElement('div');            // creates grid
 grid.className = 'grid';
@@ -10,7 +17,13 @@ for (let i = 0; i < columns; ++i) {
         let cell = document.createElement('div');    // creates cell
         cell.className = 'cell';
         column.appendChild(cell);                  // appends cell to column (as row)
+
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "black";
+        })
+        
     }
     grid.appendChild(column);                        // appends column to grid
 }
 document.getElementById("grid-container").appendChild(grid);                     // appends grid to webpage
+
